10890893529211a3851185499d2782c153e96433
Ekara28 <emirhan.karaca2024@gmail.com>
2026-06-19 09:09:55 +0200

    Add files via upload

---
+++ Snake_Klasse_Spieler/src/Spieler.java
@@ -0,0 +1,32 @@
+public class Spieler {
+
+    private int anzKoerperzellen;
+    private String Richtung;
+
+
+
+    Spieler(int anzKoerperzellen, String Richtung){
+        this.anzKoerperzellen = anzKoerperzellen;
+        this.Richtung = Richtung;
+    }
+
+
+    void Checkverloren(){
+
+    }
+
+
+    void Bewegen(){
+
+    }
+
+       void Richtung(){
+            
+    }
+
+
+
+
+
+
+}
