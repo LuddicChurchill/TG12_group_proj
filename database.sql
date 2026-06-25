b5efe0cb457734eb49745d14840ca2d6e21c1666
anavl21 <anastasiiavlasenko21@gmail.com>
2026-06-26 00:10:34 +0200

    Testdaten aus database.sql entfernt

--- database.sql
+++ database.sql
@@ -32,30 +32,4 @@ CREATE INDEX idx_spieler_name ON Spieler(name);
 CREATE INDEX idx_highscore_spieler ON Highscore(spielerID);
 CREATE INDEX idx_highscore_spiel ON Highscore(spielID);
 
-/*TESTDATEN highscore*/
-INSERT INTO SnakeSpiel (name, schwierigkeit, spieleNr) VALUES
-('Snake - das Original', 2, 1),
-('Snake II', 2, 2),
-('TG-Snake', 4, 3),
-('Snake Extreme', 5, 4),
-('Classic Snake', 1, 5),
-('Speed Snake', 3, 6);
 
-INSERT INTO Spieler (name, passwort, spielerNr) VALUES
-('max_mustermann', 'test123', 1),
-('lisa_schmidt', 'test123', 2),
-('tom_weber', 'test123', 3),
-('anna_klein', 'test123', 4),
-('admin', 'admin123', 0);
-
-INSERT INTO Highscore (spielerID, spielID, highscore) VALUES
-(1, 1, 82),
-(1, 2, 45),
-(1, 3, 4242),
-(2, 1, 120),
-(2, 2, 67),
-(2, 4, 300),
-(3, 1, 55),
-(3, 5, 200),
-(4, 3, 1500),
-(4, 6, 80);
