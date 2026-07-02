aac0e1496daedcdd51fb9bf7f23476409fa3492d
Ekara28 <emirhan.karaca2024@gmail.com>
2026-07-02 08:44:30 +0200

    did shit

--- Snake_Klasse_Spieler/src/Spieler.java
+++ Snake_Klasse_Spieler/src/Spieler.java
@@ -1,32 +1,49 @@
 public class Spieler {
 
-    private int anzKoerperzellen;
-    private String Richtung;
+    private int spielernr;
+    private String name;
+    private int id;
+    private String passwort;
 
 
 
-    Spieler(int anzKoerperzellen, String Richtung){
-        this.anzKoerperzellen = anzKoerperzellen;
-        this.Richtung = Richtung;
+    Spieler(int spielernr, String passwort, String name, int id){
+        this.spielernr=spielernr;
+        this.passwort=passwort;
+        this.name=name;
+        this.id=id;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 
-    void Checkverloren(){
-
+    public int getSpielernr() {
+        return spielernr;
     }
 
-
-    void Bewegen(){
-
+    public void setSpielernr(int spielernr) {
+        this.spielernr = spielernr;
     }
-
-       void Richtung(){
-            
-    }
-
-
-
-
-
-
 }
