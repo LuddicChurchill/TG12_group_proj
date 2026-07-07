6e2da1a1fd4b07e420561467bdf523ffe52dac43
LuddicChurchill <helena.a.schuster@gmail.com>
2026-07-07 14:51:51 +0200

    finished(?) DatabaseInterface

--- Snake_Klasse_Spieler/src/IO/DatabaseInterface.java
+++ Snake_Klasse_Spieler/src/IO/DatabaseInterface.java
@@ -1,5 +1,44 @@
 package IO;
 
-public class DatabaseInterface {
+import java.sql.*;
 
+public class DatabaseInterface {
+    String url = "";
+    String user = "root";
+    String pass = "";
+
+    Connection connection = null;
+
+
+    public DatabaseInterface(String url, String user, String pass) {
+        this.url = url;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public void establishConnection() {
+        try{
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ResultSet executeQuery(String query){
+        try{
+            Statement stm = connection.createStatement();
+            return stm.executeQuery(query);
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public void closeConnection(){
+        try{
+            connection.close();
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
