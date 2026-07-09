85f96a0d4d81185588eb542441b90b6d445dcca1
Ekara28 <emirhan.karaca2024@gmail.com>
2026-07-09 07:51:54 +0200

    did shit (again)

--- Snake_Klasse_Spieler/src/logic/Spieler.java
+++ Snake_Klasse_Spieler/src/logic/Spieler.java
@@ -1,5 +1,10 @@
 package logic;
 
+import IO.DatabaseInterface;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Spieler {
 
     private int spielernr;
@@ -13,6 +18,21 @@ public class Spieler {
         this.passwort = passwort;
         this.name = name;
         this.id = id;
+    }
+
+    public static boolean login(DatabaseInterface dbi, String name, String passwort) throws SQLException {
+        String query = "SELECT passwort FROM spieler WHERE benutzername = " + name;
+        ResultSet rs = dbi.executeQuery(query);
+
+        if (rs.next()) { // Benutzer gefunden
+            String gespeichertesPasswort = rs.getString("passwort");
+            return gespeichertesPasswort.equals(passwort);
+        }
+        else {
+        return false; // Benutzer nicht gefunden oder Fehler
+    }
+
+
     }
 
     public String getPasswort() {
