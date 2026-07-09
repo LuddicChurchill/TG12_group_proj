8bcea74569260381127a059b51271ff8666c811c
Ekara28 <emirhan.karaca2024@gmail.com>
2026-07-09 09:07:46 +0200

    3 neue Funktionen hinzugefügt:
    - Login
    - getHighscore
    - getLeaderboard

--- Snake_Klasse_Spieler/src/logic/Spieler.java
+++ Snake_Klasse_Spieler/src/logic/Spieler.java
@@ -2,8 +2,12 @@ package logic;
 
 import IO.DatabaseInterface;
 
+import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Spieler {
 
@@ -11,17 +15,19 @@ public class Spieler {
     private String name;
     private int id;
     private String passwort;
+    private int highscore;
 
 
-    Spieler(int spielernr, String passwort, String name, int id) {
+    Spieler(int spielernr, String passwort, String name, int id, int highscore) {
         this.spielernr = spielernr;
         this.passwort = passwort;
         this.name = name;
         this.id = id;
+        this.highscore = highscore;
     }
 
     public static boolean login(DatabaseInterface dbi, String name, String passwort) throws SQLException {
-        String query = "SELECT passwort FROM spieler WHERE benutzername = " + name;
+        String query = "SELECT passwort FROM spieler WHERE name = " + name;
         ResultSet rs = dbi.executeQuery(query);
 
         if (rs.next()) { // Benutzer gefunden
@@ -34,6 +40,44 @@ public class Spieler {
 
 
     }
+
+    public int getHighscore(DatabaseInterface dbi,int spielID, int spielerID) throws SQLException {
+        String query = "SELECT highscore FROM score WHERE spielID = "
+                + spielID + " AND spielerID = " + spielerID;
+
+        ResultSet rs = dbi.executeQuery(query);
+
+        try {
+            if (rs.next()) {
+                return rs.getInt("highscore");
+            } else {
+                return -1;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Spieler> getLeaderboard(DatabaseInterface dbi, int spielId, int amount) throws SQLException {
+        List<Spieler> leaderboard = new ArrayList<>();
+        String query = "SELECT s.id, s.name, s.passwort, s.spielerNr, h.highscore " + "FROM Highscore h " + "JOIN Spieler s ON h.spielerID = s.id " + "WHERE h.spielID = " + spielId + " ORDER BY h.highscore DESC " + " LIMIT " + amount;
+
+        ResultSet rs = dbi.executeQuery(query);
+
+        while (rs.next()) {
+            Spieler spieler = new Spieler(
+                    rs.getInt("spielerNr"),
+                    rs.getString("passwort"),
+                    rs.getString("name"),
+                    rs.getInt("id"),
+                    rs.getInt("highscore")
+            );
+
+                leaderboard.add(spieler);
+            }
+
+            return leaderboard;
+        }
 
     public String getPasswort() {
         return passwort;
@@ -66,5 +110,12 @@ public class Spieler {
 
     public void setSpielernr(int spielernr) {
         this.spielernr = spielernr;
+    }
+    public int getHighscore() {
+        return highscore;
+    }
+
+    public void setHighscore(int highscore) {
+        this.highscore = highscore;
     }
 }
